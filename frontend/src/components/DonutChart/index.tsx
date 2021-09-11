4c3c61fe0b80f09e683f463b731f2b35edd7e7db
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2021-09-11 20:17:31 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/requests";
@@ -10,23 +11,24 @@ type ChartData = {
 
 // https://apexcharts.com/react-chart-demos/pie-charts/
 const DonutChart = () => {
-  let chartData: ChartData = { labels: [], series: [] };
-
-  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
-    const data = response.data as SaleSum[];
-    const myLabels = data.map((x) => x.sellerName);
-    const mySeries = data.map((x) => x.sum);
-
-    chartData = { labels: myLabels, series: mySeries };
-
-    console.log(chartData);
+  // Hook: useState Manter estado no componente
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
   });
 
-  // const mockData = {
-  //   series: [477138, 499928, 444867, 220426, 473088],
-  //   labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  // };
+  // Hook: useEffect Executar algo na instanciação ou destruição do componente, observar estado
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
 
+      setChartData({ labels: myLabels, series: mySeries });
+    });
+  }, []);
+
+  //consigurações do grafico
   const options = {
     legend: {
       show: true,
