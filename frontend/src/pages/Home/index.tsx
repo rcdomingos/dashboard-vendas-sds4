6933ad5c081ee6677b99d93b0eb55f6975186332
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2021-09-11 18:47:17 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,27 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Pagina home</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container py-5">
+        <h1 className="display-4 fw-bold">CSVendas</h1>
+        <p className="lead">
+          Analise o desempenho das suas vendas por diferentes perspectivas
+        </p>
+        <hr />
+        <p className="col-md-8 fs-5">
+          Esta aplicação consiste em exibir um dashboard a partir de dados
+          fornecidos por um back end construído com Spring Boot.
+        </p>
+        <Link className="btn btn-primary btn-lg" to="dashboard">
+          Acessar dashboard
+        </Link>
+      </div>
+      <Footer />
+    </>
   );
 };
 
